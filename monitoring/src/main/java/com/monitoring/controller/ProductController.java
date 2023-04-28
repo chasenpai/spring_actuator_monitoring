51bf7c94aab7aa65bd2d96bc062447edd82f73da
junwvwv <wvwv0612@gmail.com>
2023-04-28 23:50:24 +0900

    product-monitoring 카운팅 메트릭 생성

--- monitoring/src/main/java/com/monitoring/controller/ProductController.java
+++ monitoring/src/main/java/com/monitoring/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.monitoring.controller;
 
 import com.monitoring.response.ProductResource;
 import com.monitoring.service.ProductService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.MediaTypes;
@@ -21,14 +23,28 @@ import java.util.stream.Collectors;
 public class ProductController {
 
     private final ProductService productService;
+    private final MeterRegistry meterRegistry;
 
     @GetMapping("/v1/products/{id}")
     public ProductResource getProduct(@PathVariable("id") Long id){
+        /**
+         * /monitor/metrics/product-monitoring - 메트릭이 product-monitoring 인 모든 method 태그를 출력
+         * 특정 태그에 대한 메트릭 출력 시 엔드포인트 URL 에 매개변수를 추가
+         * /monitor/metrics/product-monitoring?tag=method:get product
+         */
+        Counter.builder("product-monitoring")
+                .tag("method", "get product")
+                .description("get product")
+                .register(meterRegistry).increment();
         return new ProductResource(productService.getProduct(id));
     }
 
     @GetMapping("/v1/products")
     public List<ProductResource> getAllProducts(){
+        Counter.builder("product-monitoring")
+                .tag("method", "get product list")
+                .description("get product list")
+                .register(meterRegistry).increment();
         return productService.getAllProducts().stream()
                 .map(ProductResource::new)
                 .collect(Collectors.toList());
