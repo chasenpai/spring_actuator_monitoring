959f120b05de2ccc4cbe996b6900ddfdac2b37f5
junwvwv <wvwv0612@gmail.com>
2023-04-29 18:53:54 +0900

    주석 추가, 설정 변경

--- monitoring/src/main/java/com/monitoring/controller/ProductController.java
+++ monitoring/src/main/java/com/monitoring/controller/ProductController.java
@@ -51,4 +51,32 @@ public class ProductController {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * 주요 엔드포인트
+     * beans - 스프링 컨테이너에 등록된 스프링 빈 표시
+     * conditions - condition 을 통해서 빈을 등록할 때 평가 조건과 일치하거나 일치하지 않는 이유를 표시
+     * configprops - @ConfigurationProperties 를 표시
+     * env - Environment 정보를 표시 > info. 으로 시작하는 정보를 출력
+     * health - 애플리케이션 헬스 정보 표시
+     * httpexchanges - HTTP 호출 응답 정보를 표시 > 별도로 HttpExchangeRepository 구현 빈 등록 필요
+     * info - 애플리케이션 정보 표시
+     * loggers - 애플리케이션 로거 설정 표시 > 실시간 변경 가능
+     * metrics - 애플리케이션 메트릭 정보를 표시
+     * mappings - @RequestMapping 정보릂 표시
+     * threaddump - 쓰레드 덤프 실행 및 표시
+     * shutdown - 애플리케이션을 종료 > 기본적으로 비활성화
+     */
+
+    /**
+     * 애플리케이션 종료 시키기
+     * - /monitor/shutdown + Post 요청
+     */
+
+    /**
+     * /monitor/loggers/com.monitoring.controller - 특정 로거
+     * 실시간 로그 레벨 변경 가능 - 특정 로거 url + post 요청 + json
+     * {
+     *    "configuredLevel": "TRACE"
+     * }
+     */
 }
